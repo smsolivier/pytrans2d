69f923eb75c2c27484f9ca6c92be38aeb6b85350
Sam Olivier <smsolivier@gmail.com>
2020-06-01 15:38:52 -0700

    plots for will and ben

--- exe/lor.py
+++ exe/lor.py
@@ -5,13 +5,17 @@ import matplotlib.pyplot as plt
 import sys 
 
 from trans2d import * 
+from OutputCycler import OutputCycler
 
-Ne = 10 
-p = 3 
-if (len(sys.argv)>1):
-	Ne = int(sys.argv[1])
-if (len(sys.argv)>2):
-	p = int(sys.argv[2]) 
+oc = OutputCycler()
+p = oc.GetOpt(0, 2)
+sm_inner = oc.GetOpt(1, 1)
+
+# Ne = 10 
+# if (len(sys.argv)>1):
+# 	Ne = int(sys.argv[1])
+# if (len(sys.argv)>2):
+# 	p = int(sys.argv[2]) 
 
 # mesh = RectMesh(Ne, Ne) 
 # space = H1Space(mesh, LobattoBasis, p) 
@@ -42,89 +46,88 @@ if (len(sys.argv)>2):
 # solver.Solve(K, K, f)
 # print('amg: it={}, norm={:.3e}'.format(solver.it, solver.norm))
 
-mesh = RectMesh(Ne, Ne)
-h1 = H1Space(mesh, LobattoBasis, p, 2)
-l2 = L2Space(mesh, LegendreBasis if p>1 else LegendreBasis, p-1, 1, False)
-h1l = h1.LORefine()
-l2l = L2Space(h1l.mesh, LegendreBasis, 0, 1, False)
-print('Nu = {}'.format(h1.Nu + l2.Nu))
-# l2.Plot()
-# l2l.Plot()
-# h1.Plot()
-# h1l.Plot()
-# plt.show()
+Ne = np.array([4, 8, 12, 16, 20])
+it = np.zeros((4,len(Ne)))
 
-bnodes = []
-for bn in h1.bnodes:
-	if (h1.nodes[bn,1]==1 or h1.nodes[bn,1]<1e-3):
-		bnodes.append(int(h1.Nu/2) + bn) 
+maxiter = 500
+amg_inner = 1
+for i in range(len(Ne)):
+	mesh = RectMesh(Ne[i], Ne[i])
+	h1 = H1Space(mesh, LobattoBasis, p, 2)
+	l2 = L2Space(mesh, LegendreBasis if p>1 else LegendreBasis, p-1, 1, False)
+	h1l = h1.LORefine()
+	l2l = L2Space(h1l.mesh, LegendreBasis, 0, 1, False)
+	print('Nu = {}'.format(h1.Nu + l2.Nu))
+	# l2.Plot()
+	# l2l.Plot()
+	# h1.Plot()
+	# h1l.Plot()
+	# plt.show()
 
-Mt = Assemble(h1, VectorMassIntegrator, lambda x: 1, 2*p+1)
-MtLump = Assemble(h1, VectorMassIntegratorRowSum, lambda x: 1, 2*p+1)
-MtLumpInv = sp.diags(1/MtLump.diagonal())
-Ma = Assemble(l2, MassIntegrator, lambda x: .1, 2*p+1)
-D = MixAssemble(l2, h1, MixDivIntegrator, 1, 2*p+1)
-DT = D.transpose()
-# f = AssembleRHS(l2, DomainIntegrator, 
-	# lambda x: (2*np.pi**2+.1)*np.sin(np.pi*x[0])*np.sin(np.pi*x[1]), 2*p+1)
-f = AssembleRHS(l2, DomainIntegrator, lambda x: (np.pi**2 + .1)*np.sin(np.pi*x[0]), 2*p+1)
-# f = AssembleRHS(l2, DomainIntegrator, lambda x: 1, 2*p+1)
+	bnodes = []
+	for bn in h1.bnodes:
+		if (h1.nodes[bn,1]==1 or h1.nodes[bn,1]<1e-3):
+			bnodes.append(int(h1.Nu/2) + bn) 
 
-M = sp.bmat([[Mt, -DT], [D, Ma]]).tolil()
-M[bnodes,:] = 0 
-M[bnodes,bnodes] = 1 
-M = M.tocsc()
-rhs = np.concatenate((np.zeros(h1.Nu), f))
-rhs[bnodes] = 0 
-S = Ma + D*MtLumpInv*DT 
+	sigt = lambda x: (x[0]>=.5)*100 + (x[0]<.5)*1 
+	siga = lambda x: (x[0]>=.5)*1 + (x[0]<.5)*.1 
+	Mt = Assemble(h1, VectorMassIntegrator, sigt, 2*p+1)
+	MtLump = Assemble(h1, VectorMassIntegratorRowSum, sigt, 2*p+1)
+	MtLumpInv = sp.diags(1/MtLump.diagonal())
+	Ma = Assemble(l2, MassIntegrator, siga, 2*p+1)
+	D = MixAssemble(l2, h1, MixDivIntegrator, 1, 2*p+1)
+	DT = D.transpose()
+	# f = AssembleRHS(l2, DomainIntegrator, 
+		# lambda x: (2*np.pi**2+.1)*np.sin(np.pi*x[0])*np.sin(np.pi*x[1]), 2*p+1)
+	# f = AssembleRHS(l2, DomainIntegrator, lambda x: (np.pi**2 + .1)*np.sin(np.pi*x[0]), 2*p+1)
+	f = AssembleRHS(l2, DomainIntegrator, lambda x: 1, 2*p+1)
 
-Mtl = Assemble(h1l, VectorMassIntegrator, lambda x: 1, 2*p+1)
-MtlLump = Assemble(h1l, VectorMassIntegratorRowSum, lambda x: 1, 2*p+1)
-MtlLumpInv = sp.diags(1/MtlLump.diagonal())
-Mal = Assemble(l2l, MassIntegrator, lambda x: .1, 2*p+1)
-Dl = MixAssemble(l2l, h1l, MixDivIntegrator, 1, 2*p+1)
-DlT = Dl.transpose()
-fl = AssembleRHS(l2l, DomainIntegrator, 
-	lambda x: (np.pi**2+.1)*np.sin(np.pi*x[0]), 2*p+1)
+	M = sp.bmat([[Mt, -DT], [D, Ma]]).tocsc()
+	rhs = np.concatenate((np.zeros(h1.Nu), f))
+	S = Ma + D*MtLumpInv*DT 
 
-Ml = sp.bmat([[Mtl, -DlT], [Dl, Mal]]).tolil()
-Ml[bnodes,:] = 0 
-Ml[bnodes,bnodes] = 1
-Ml = Ml.tocsc()
-rhsl = np.concatenate((np.zeros(h1.Nu), fl))
-rhsl[bnodes] = 0 
-Sl = Mal + Dl*MtLumpInv*Dl.transpose()
+	Mtl = Assemble(h1l, VectorMassIntegrator, sigt, 2*p+1)
+	MtlLump = Assemble(h1l, VectorMassIntegratorRowSum, sigt, 2*p+1)
+	MtlLumpInv = sp.diags(1/MtlLump.diagonal())
+	Mal = Assemble(l2l, MassIntegrator, siga, 2*p+1)
+	Dl = MixAssemble(l2l, h1l, MixDivIntegrator, 1, 2*p+1)
+	DlT = Dl.transpose()
 
-# Shat = Mal + Dl*MtlLumpInv*DlT
-# Shat = Ma + D*MtLumpInv*DT 
-# L = sp.bmat([[sp.identity(h1.Nu), None], [D*MtLumpInv, sp.identity(l2.Nu)]])
-# D = sp.bmat([[Mt, None], [None, Shat]])
-# U = sp.bmat([[sp.identity(h1.Nu), -MtLumpInv*DT], [None, sp.identity(l2.Nu)]])
-# P = (L*D*U).tocsc()
+	Ml = sp.bmat([[Mtl, -DlT], [Dl, Mal]]).tocsc()
+	Sl = Mal + Dl*MtLumpInv*Dl.transpose()
 
-# prec = spla.inv(Sl)*S 
-# print('kappa(prec) = {:.3f}'.format(np.linalg.cond(prec.todense())))
-# print('kappa(S) = {:.3f}'.format(np.linalg.cond(S.todense())))
+	smoother = SymGaussSeidel(1e-10, sm_inner, False)
+	solver = AMGSolver(1e-10, maxiter, amg_inner, smoother, False)
+	solver.Solve(S, Sl, f)
+	print('sgs = {}'.format(solver.it))
+	it[0,i] = solver.it 
 
-prec = spla.inv(Ml)*M
-print('kappa(prec) = {:.3f}'.format(np.linalg.cond(prec.todense())))
-print('kappa(M) = {:.3f}'.format(np.linalg.cond(M.todense())))
-print('kappa(Ml) = {:.3f}'.format(np.linalg.cond(Ml.todense())))
+	smoother = GaussSeidel(1e-10, sm_inner, False)
+	solver = AMGSolver(1e-10, maxiter, amg_inner, smoother, False)
+	solver.Solve(S, Sl, f)
+	print('gs = {}'.format(solver.it))
+	it[1,i] = solver.it 
 
-# prec = spla.inv(Sl)*S 
-# print('kappa(Sprec) = {:.3f}'.format(np.linalg.cond(prec.todense())))
-# print('kappa(S) = {:.3f}'.format(np.linalg.cond(S.todense())))
+	smoother = Jacobi(1e-10, sm_inner, False)
+	solver = AMGSolver(1e-10, maxiter, amg_inner, smoother, False)
+	solver.Solve(S, Sl, f)
+	print('jacobi = {}'.format(solver.it))
+	it[2,i] = solver.it 
 
-# x = spla.spsolve(M, rhs)
-# T = GridFunction(l2)
-# T.data = x[h1.Nu:]
+	solver = AMGSolver(1e-10, maxiter, amg_inner, None, False)
+	solver.Solve(S, Sl, f)
+	print('none = {}'.format(solver.it))
+	it[3,i] = solver.it 
 
-# xl = spla.spsolve(Ml, rhsl)
-# Tl = GridFunction(l2l)
-# Tl.data = x[h1.Nu:]
-
-# err = Tl.L2Error(lambda x: np.sin(np.pi*x[0]), 2*p+2)
-# print('err = {:.3e}'.format(err))
-
-# Tl.space.mesh.WriteVTK('solution', cell={'T':Tl.ElementData()})
-# # T.space.mesh.WriteVTK('solution', cell={'T':T.ElementData()})
+plt.plot(Ne**2, it[3], '-o', label='None')
+plt.plot(Ne**2, it[2], '-o', label='Jacobi')
+plt.plot(Ne**2, it[1], '-o', label='GS')
+plt.plot(Ne**2, it[0], '-o', label='Sym. GS')
+plt.xlabel('Number of Elements')
+plt.ylabel('GMRES Iterations')
+plt.title(r'$p='+str(p)+'$, smoother it=' + str(sm_inner))
+plt.legend()
+if (oc.Good()):
+	plt.savefig(oc.Get())
+else:
+	plt.show()

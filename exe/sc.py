54190fd93d5ee5315f1d1a7c491404378b129cf0
Sam Olivier <smsolivier@gmail.com>
2020-11-18 12:57:02 -0800

    updated sc

--- exe/sc.py
+++ exe/sc.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 from trans2d import * 
 from scipy.integrate import solve_ivp 
 
+from OutputCycler import OutputCycler
+oc = OutputCycler()
+
 def RayTrace(trans, ip, Omega):
 	found = False 
 	IP = trans.Transform(ip) 
@@ -40,7 +43,7 @@ def RayTrace(trans, ip, Omega):
 	return xi 
 
 def ForwardEuler(f, tspan, y0):
-	Nx = 100
+	Nx = 2**(p+1)
 	dx = (tspan[1] - tspan[0])/Nx 
 	x = tspan[0] 
 	for n in range(Nx):
@@ -60,9 +63,9 @@ def SCSolve(el, trans, ip, Omega, b):
 		return np.array([(Omega[1]*F[0,0] - Omega[0]*F[1,0])/(Omega[0]*F[1,1] - Omega[1]*F[0,1]), 
 			J/(Omega[0]*F[1,1] - Omega[1]*F[0,1])*(q - u[1])])
 	x0 = RayTrace(trans, ip, Omega)
-	# sol = solve_ivp(rhs, [x0[0], ip[0]], y0=[x0[1], inflow(x0, Omega)], rtol=1e-10, atol=1e-10)
+	# sol = solve_ivp(rhs, [x0[0], ip[0]], y0=[x0[1], 0], rtol=1e-12, atol=1e-12)
 	# return sol.y[1,-1] 
-	return ForwardEuler(rhs, [x0[0], ip[0]], [x0[1], inflow(x0, Omega)])[1]
+	return ForwardEuler(rhs, [x0[0], ip[0]], [x0[1], 0])[1]
 
 def SCSolveSource(el, trans, ip, Omega, source):
 	def rhs(x,u):
@@ -86,82 +89,98 @@ def Exact(x):
 		# return np.exp(-1/Omega[1]*x[1])
 		return (inflow(x,Omega) - x[0] + Omega[0]/Omega[1]*x[1] + Omega[0])*np.exp(-x[1]/Omega[1]) + x[0] - Omega[0]
 
-p = 4
+p = oc.GetOpt(0,3) 
 N = 1 
 mesh = RectMesh(N,N, [0,0], [1,1])
 trans = mesh.trans[0]
-space = L2Space(mesh, LegendreBasis, p) 
-m = MomentBasis(space.el.basis) 
+orders = np.arange(1, 12)
+itse = [] 
+itsp = []
+for p in orders:
+	space = L2Space(mesh, LegendreBasis, p) 
+	m = MomentBasis(space.el.basis) 
 
-Omega = np.array([1,.25])
-inflow = lambda x, Omega: 1
-G = Assemble(space, WeakConvectionIntegrator, Omega, 2*p+1)
-M = Assemble(space, MassIntegrator, lambda x: 1, 2*p+1) 
-F = FaceAssembleAll(space, UpwindTraceIntegrator, Omega, 2*p+1)
-I = FaceAssembleRHS(space, InflowIntegrator, [Omega, inflow], 2*p+1)
-# source = lambda x: 16*(Omega[0]*(1-2*x[0])*(x[1]-x[1]**2) + Omega[1]*(x[0]-x[0]**2)*(1-2*x[1]) + (x[0]-x[0]**2)*(x[1] - x[1]**2))
-source = lambda x: x[0]
-q = AssembleRHS(space, DomainIntegrator, source, 2*p+1)
-I += q 
+	Omega = np.array([1,.25])
+	inflow = lambda x, Omega: 0
+	G = Assemble(space, WeakConvectionIntegrator, Omega, 2*p+1)
+	M = Assemble(space, MassIntegrator, lambda x: 1, 2*p+1) 
+	F = FaceAssembleAll(space, UpwindTraceIntegrator, Omega, 2*p+1)
+	I = FaceAssembleRHS(space, InflowIntegrator, [Omega, inflow], 2*p+1)
+	# source = lambda x: 16*(Omega[0]*(1-2*x[0])*(x[1]-x[1]**2) + Omega[1]*(x[0]-x[0]**2)*(1-2*x[1]) + (x[0]-x[0]**2)*(x[1] - x[1]**2))
+	source = lambda x: x[0]
+	q = AssembleRHS(space, DomainIntegrator, source, 2*p+1)
+	I += q 
 
-A = G + M + F
-it = 0
-norm = 0 
-def Callback(r):
-	global it 
-	global norm 
-	global prev 
-	it += 1 
-	norm = np.linalg.norm(r)
-	# print('it = {:3}, norm = {:.3e}'.format(it, norm))
+	A = G + M + F
+	it = 0
+	norm = 0 
+	def Callback(r):
+		global it 
+		global norm 
+		global prev 
+		it += 1 
+		norm = np.linalg.norm(r)
+		# print('it = {:3}, norm = {:.3e}'.format(it, norm))
 
-u = GridFunction(space)
-ip, w = quadrature.Get(3*p+1)
-M2 = MassIntegrator(space.el, mesh.trans[0], lambda x: 1, 2*p+1)
-def SCProj(b):
-	r = np.zeros(M2.shape[0])
-	for n in range(len(w)):
-		r += space.el.CalcShape(ip[n]) * SCSolve(space.el, mesh.trans[0], ip[n], Omega, b) * w[n] * trans.Jacobian(ip[n]) 
+	u = GridFunction(space)
+	ip, w = quadrature.Get(2*p+1)
+	M2 = MassIntegrator(space.el, mesh.trans[0], lambda x: 1, 2*p+1)
+	def SCProj(b):
+		r = np.zeros(M2.shape[0])
+		for n in range(len(w)):
+			r += space.el.CalcShape(ip[n]) * SCSolve(space.el, mesh.trans[0], ip[n], Omega, b) * w[n] * trans.Jacobian(ip[n]) 
 
-	return r/M2.diagonal() if type(space.el.basis)==LegendreBasis else np.linalg.solve(M2, r) 
+		return r/M2.diagonal() if type(space.el.basis)==LegendreBasis else np.linalg.solve(M2, r) 
 
-def SCEval(b):
-	r = np.zeros(space.el.Nn)
-	for n in range(space.el.Nn):
-		r[n] = SCSolve(space.el, mesh.trans[0], space.el.nodes[n], Omega, b)
+	def SCEval(b):
+		r = np.zeros(space.el.Nn)
+		for n in range(space.el.Nn):
+			r[n] = SCSolve(space.el, mesh.trans[0], space.el.nodes[n], Omega, b)
 
-	# num = 50
-	# xi = np.linspace(-1,1,num)
-	# X = np.zeros((num,num))
-	# Y = np.zeros((num,num))
-	# U = np.zeros((num,num))
-	# for i in range(num):
-	# 	for j in range(num):
-	# 		ip = np.array([xi[i], xi[j]])
-	# 		X[i,j], Y[i,j] = trans.Transform(ip)
-	# 		# U[i,j] = space.el.CalcShape(ip)@b
-	# 		U[i,j] = SCSolve(space.el, mesh.trans[0], ip, Omega, b)
+		# num = 50
+		# xi = np.linspace(-1,1,num)
+		# X = np.zeros((num,num))
+		# Y = np.zeros((num,num))
+		# U = np.zeros((num,num))
+		# for i in range(num):
+		# 	for j in range(num):
+		# 		ip = np.array([xi[i], xi[j]])
+		# 		X[i,j], Y[i,j] = trans.Transform(ip)
+		# 		# U[i,j] = space.el.CalcShape(ip)@b
+		# 		U[i,j] = SCSolve(space.el, mesh.trans[0], ip, Omega, b)
 
-	# plt.pcolor(X,Y,U)
-	# plt.colorbar()
-	# plt.show()
+		# plt.pcolor(X,Y,U)
+		# plt.colorbar()
+		# plt.show()
 
-	return r 
+		return r 
 
-# u.data = SCProj(np.zeros(space.el.Nn))
-# u.data = SCEval(np.zeros(space.el.Nn))
-u.data, info = pyamg.krylov.gmres(A.tocsc(), I, M=spla.LinearOperator(A.shape, SCEval), 
-	callback=Callback, tol=1e-10, maxiter=A.shape[0])
-print('it = {}/{}, residual = {:.3e}'.format(it, space.el.Nn, np.linalg.norm(A*u.data - I)))
-it = 0 
-u.data, info = pyamg.krylov.gmres(A.tocsc(), I, M=spla.LinearOperator(A.shape, SCProj), 
-	callback=Callback, tol=1e-10, maxiter=A.shape[0])
-print('it = {}/{}, residual = {:.3e}'.format(it, space.el.Nn, np.linalg.norm(A*u.data - I)))
+	# u.data = SCProj(np.zeros(space.el.Nn))
+	# u.data = SCEval(np.zeros(space.el.Nn))
+	# u.data, info = pyamg.krylov.fgmres(A.tocsc(), I,
+	# 	callback=Callback, tol=1e-10, maxiter=A.shape[0])
+	# print('it = {}/{}, residual = {:.3e}'.format(it, space.el.Nn, np.linalg.norm(A*u.data - I)))
+	# itse.append(it) 
+	# it = 0 
+	# u.data, info = pyamg.krylov.fgmres(A.tocsc(), I, M=spla.LinearOperator(A.shape, SCEval), 
+		# callback=Callback, tol=1e-10, maxiter=A.shape[0])
+	u.data, info = pyamg.krylov.steepest_descent(A.tocsc(), I, M=spla.LinearOperator(A.shape, SCEval), 
+		callback=Callback, tol=1e-10, maxiter=A.shape[0])
+	print('p = {}, it = {}/{}, residual = {:.3e}'.format(p, it, space.el.Nn, np.linalg.norm(A*u.data - I)))
+	itsp.append(it)
 
 
-# u.data = np.linalg.solve(A.todense(), I)
+	# u.data = np.linalg.solve(A.todense(), I)
 
-print('proj. err = {:.3e}'.format(u.L2Error(Exact, 2*p+1)))
+	# print('proj. err = {:.3e}'.format(u.L2Error(Exact, 2*p+1)))
+
+# plt.plot(orders, itse, '-o', label='Unprec')
+plt.plot(orders, itsp, '-o', label='SC')
+# plt.plot(orders, (orders+1)**2, '-o', label='Size of System')
+plt.xlabel('$p$')
+plt.ylabel('FGMRES Iterations')
+plt.legend()
+plt.show()
 # err = 0
 # for n in range(len(w)):
 # 	x = trans.Transform(ip[n])

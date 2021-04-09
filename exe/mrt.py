293184ac0bd98f58ab88723a0965c9370f3de71d
Sam Olivier <smsolivier@gmail.com>
2021-04-09 16:04:50 -0700

    fixed MRT. Eddington tensor needed to be rotated to match the rotated mesh

--- exe/mrt.py
+++ exe/mrt.py
@@ -20,9 +20,9 @@ mesh = RectMesh(Ne, Ne)
 # 		if (mesh.nodes[i,j]>0 and mesh.nodes[i,j]<1):
 # 			mesh.nodes[i,j] = np.random.normal(mesh.nodes[i,j], h*.1)
 
-bc = 0
-theta = np.pi/6
-# theta = 0
+bc = 1
+# theta = np.pi/6
+theta = 0
 rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta),np.cos(theta)]])
 mesh.nodes = mesh.nodes@rot.T 
 mesh = AbstractMesh(mesh.nodes, mesh.ele, 1)
@@ -127,8 +127,6 @@ def VectorFEJumpAvg(el1, el2, face, c, qorder):
 		s1 = el2[0].CalcShape(xi1)
 		s2 = el2[1].CalcShape(xi2)
 
-		cn = coef@nor
-		beta = cn@tau
 		F1 = face.trans1.F(xi1)
 		J1 = np.linalg.det(F1)
 		F2 = face.trans2.F(xi2)
@@ -136,10 +134,13 @@ def VectorFEJumpAvg(el1, el2, face, c, qorder):
 		Fi1 = np.linalg.inv(F1)
 		Fi2 = np.linalg.inv(F2)
 		Jf = face.face.Jacobian(ip[n])
-		cov1 = vs1.T@Fi1@tau
-		cov2 = vs2.T@Fi2@tau
 
-		j = np.concatenate((beta*cov1, -beta*cov2))
+		cn = coef@nor
+		beta = cn@tau
+		contra1 = 1/J1*vs1.T@F1.T@tau 
+		contra2 = 1/J2*vs2.T@F2.T@tau 
+
+		j = np.concatenate((beta*contra1, -beta*contra2))
 		a = np.concatenate((s1, s2)) * (1 if face.boundary else .5) 
 		elmat += np.outer(j,a) * w[n] * Jf
 
@@ -151,22 +152,25 @@ Ma = Assemble(l2, MassIntegrator, lambda x: 0, 2*(p+1)+1)
 diag = Ml.diagonal() 
 Mlinv = sp.diags(1/diag)
 alpha = 1/3
-beta = 1/10
+beta = 1
 # beta = 0
 def R(x):
 	x = rot.T@x
-	# offt = beta*np.sin(2*np.pi*x[0])*np.sin(2*np.pi*x[1]) 
-	# offb = 15*(1 + np.sin(np.pi*x[0])*np.sin(np.pi*x[1]))
-	# return np.array([[1./3, offt/offb], [offt/offb, 1./3]])
-	return np.array([[alpha, beta], [beta, alpha]])
+	offt = beta*np.sin(2*np.pi*x[0])*np.sin(2*np.pi*x[1]) 
+	offb = 15*(1 + np.sin(np.pi*x[0])*np.sin(np.pi*x[1]))
+	r = np.array([[1./3, offt/offb], [offt/offb, 1./3]])
+	# r = np.array([[alpha, beta], [beta, alpha]])
+	return rot@r@rot.T
+	# return r
+
 # R = lambda x: np.eye(2)
 D = MixAssemble(l2, rt, VectorFEDivIntegrator, 1, 2*(p+1)+1)
 G = MixAssemble(rt, l2, VectorFEDivIntegrator2, R, 2*(p+1)+1)
 F = MixFaceAssemble(rt, l2, VectorFEJumpAvg, R, 2*(p+2)+1)
 def Q(x):
 	x = rot.T@x 
-	# return np.pi**2*(beta*-8/15*np.cos(2*np.pi*x[0])*np.cos(2*np.pi*x[1]) + 2/3*np.sin(np.pi*x[0])*np.sin(np.pi*x[1]))
-	return 2*np.pi**2*(-beta*np.cos(np.pi*x[0])*np.cos(np.pi*x[1]) + alpha*np.sin(np.pi*x[0])*np.sin(np.pi*x[1]))
+	return np.pi**2*(beta*-8/15*np.cos(2*np.pi*x[0])*np.cos(2*np.pi*x[1]) + 2/3*np.sin(np.pi*x[0])*np.sin(np.pi*x[1]))
+	# return 2*np.pi**2*(-beta*np.cos(np.pi*x[0])*np.cos(np.pi*x[1]) + alpha*np.sin(np.pi*x[0])*np.sin(np.pi*x[1]))
 # Q = lambda x: 2*np.pi**2*np.sin(np.pi*x[0])*np.sin(np.pi*x[1])
 f = AssembleRHS(l2, DomainIntegrator, Q, 2*(p+1)+1) 
 g = FaceAssembleRHS(rt, RTBdrIntegrator, lambda x: R(x)*bc, 2*(p+2)+1)
